f360413c9d3d6cf7957eb4e5afbc91edf67dc55b
sindremag <sindre.magnussen@icloud.com>
2025-01-21 13:30:47 +0100

    change the hello.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello, Class! iam react app running on azure. i was deployed using a very complexe pipeline.
         </p>
         <a
           className="App-link"
