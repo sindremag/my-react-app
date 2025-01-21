8a8d588620c69ff42e40da2aa56d3aa3b92a4717
sindremag <sindre.magnussen@icloud.com>
2025-01-21 14:08:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Class "changed1"! iam react app running on azure. i was deployed using a very complexe pipeline.
+          Hello, Class "new change"! iam react app running on azure. i was deployed using a very complexe pipeline.
         </p>
         <a
           className="App-link"
