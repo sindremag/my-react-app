e8040c3a083c4e7d158bd475c35758d829aea5dc
sindremag <sindre.magnussen@icloud.com>
2025-01-21 14:15:21 +0100

    Update App.js
    New line chart

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
         <p>
           Hello, Class "new change"! iam react app running on azure. i was deployed using a very complexe pipeline.
         </p>
+        <LineChart width={500} height={300} data={data}>
+    <XAxis dataKey="name"/>
+    <YAxis/>
+    <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+  </LineChart>
         <a
           className="App-link"
           href="https://reactjs.org"
